9fb906fcf6fb5607f83953810cf3085e97572cbd
Dominik <dominikzygarski@MacBook-Pro-Dominik.local>
2025-04-25 13:32:05 +0200

    fix: add dynamic baseUrl for Open Graph metadata in dev and production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,32 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const isProd = process.env.NODE_ENV === "production";
+
+const baseUrl = isProd
+  ? "https://www.dominikzygarski.com"
+  : "https://dev.d27y48kw4ohp7k.amplifyapp.com";
+
 export const metadata: Metadata = {
   title: "Portfolio",
   description: "Portfolio of Dominik Zygarski – DevOps & Cloud Engineer specialized in AWS, CI/CD, Infrastructure as Code, and Big Data projects.",
   icons: {
     icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Dominik Zygarski Portfolio",
+    description: "DevOps Engineer specializing in AWS, CI/CD and Cloud.",
+    url: baseUrl, // lub localhost, jeśli lokalnie
+    siteName: "Dominik Zygarski",
+    images: [
+      {
+        url: `${baseUrl}/og-image.png`,
+        width: 1200,
+        height: 630,
+        alt: "Dominik Zygarski – DevOps Engineer",
+      },
+    ],
+    type: "website",
   },
 };
 
