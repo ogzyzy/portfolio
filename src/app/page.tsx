1dfa30903d33f337ca9ed1f04e4887165a85130d
Dominik <dominikzygarski@MacBook-Pro-Dominik.local>
2025-05-19 16:19:06 +0200

    updating summary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,8 +95,9 @@ export default function Portfolio() {
                 <h1 className="text-3xl md:text-4xl font-bold mb-1 text-white">Dominik Zygarski</h1>
                 <h2 className="text-xl text-[#00B2C8] mb-4">DevOps Engineer</h2>
                 <p className="max-w-2xl text-gray-400 mb-4">
-                I’m passionate about solving complex problems using cloud technologies and automation. <br /> I focus on AWS, IaC, and CI/CD. 
-                Currently expanding into Big Data and building hands-on projects to strengthen my skills and explore real-world data challenges.
+                I'm passionate about solving complex problems using cloud technologies and automation. 
+                I focus on AWS, Infrastructure as Code, and CI/CD, with a strong ambition to grow into Cloud Data Engineering and DevOps roles. 
+                Currently, I’m expanding my skill set in Big Data by building hands-on projects that address real-world data challenges in cloud-based systems.
                 </p>
 
                 <div className="flex flex-wrap justify-center md:justify-start gap-4 mb-4">
